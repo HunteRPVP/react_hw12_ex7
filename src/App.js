31562f7359885b87f51723a099bf7f18494bd008
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-07-05 19:33:25 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const useValidation = (value, validations) => {
+  const [isEmpty, setEmpty] = useState(true);
+  const [minLengthError, setMinLengthError] = useState(false);
+  const [maxLengthError, setMaxLengthError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [inputValid, setInputValid] = useState(false);
+
+  useEffect(() => {
+    for (const validation in validations) {
+      switch (validation) {
+        case "minLength":
+          value.length < validations[validation]
+            ? setMinLengthError(true)
+            : setMinLengthError(false);
+          break;
+        case "isEmpty":
+          value ? setEmpty(false) : setEmpty(true);
+          break;
+        case "maxLength":
+          value.length > validations[validation]
+            ? setMaxLengthError(true)
+            : setMaxLengthError(false);
+          break;
+        case "isEmail":
+          const re =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          re.test(String(value).toLowerCase())
+            ? setEmailError(false)
+            : setEmailError(true);
+          break;
+        default:
+          break;
+      }
+    }
+  }, [value, validations]);
+
+  useEffect(() => {
+    if (isEmpty || maxLengthError || minLengthError || emailError) {
+      setInputValid(false);
+    } else {
+      setInputValid(true);
+    }
+  }, [isEmpty, maxLengthError, minLengthError, emailError]);
+
+  return {
+    isEmpty,
+    minLengthError,
+    maxLengthError,
+    emailError,
+    inputValid,
+  };
+};
+
+const useInput = (initialValue, validations) => {
+  const [value, setValue] = useState(initialValue);
+  const [isDirty, setDirty] = useState(false);
+  const valid = useValidation(value, validations);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onBlur = () => {
+    setDirty(true);
+  };
+
+  return {
+    value,
+    onChange,
+    onBlur,
+    isDirty,
+    ...valid,
+  };
+};
+
+const App = () => {
+  const email = useInput("", { isEmpty: true, minLength: 3, isEmail: true });
+  const password = useInput("", { isEmpty: true, minLength: 5, maxLength: 8 });
+
+  return (
+    <div className="App">
+      <form>
+        <h1>Регистрация</h1>
+        <input
+          onChange={(e) => email.onChange(e)}
+          onBlur={email.onBlur}
+          value={email.value}
+          name="email"
+          type="text"
+          placeholder="Enter your email..."
+        />
+        {email.isDirty && email.isEmpty && (
+          <div style={{ color: "red" }}>Поле не может быть пустым</div>
+        )}
+        {email.isDirty && email.minLengthError && (
+          <div style={{ color: "red" }}>Некорректная длина</div>
+        )}
+        {email.isDirty && email.emailError && (
+          <div style={{ color: "red" }}>Некорректный email</div>
+        )}
+        <br />
+        <input
+          onChange={(e) => password.onChange(e)}
+          onBlur={password.onBlur}
+          value={password.value}
+          name="password"
+          type="text"
+          placeholder="Enter your password..."
+        />
+        {password.isDirty && password.isEmpty && (
+          <div style={{ color: "red" }}>Поле не может быть пустым</div>
+        )}
+        {password.isDirty && password.minLengthError && (
+          <div style={{ color: "red" }}>Некорректная длина</div>
+        )}
+        {password.isDirty && password.maxLengthError && (
+          <div style={{ color: "red" }}>Слишком длинный пароль</div>
+        )}
+        <br />
+        <button
+          disabled={!email.inputValid || !password.inputValid}
+          type="submit"
+        >
+          Registration
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
